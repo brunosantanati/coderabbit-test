2c9ebcb54442af32cbb1deb3c355bc0d2eba1ab2
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2025-08-29 02:44:27 -0300

    Initial commit

---
+++ src/main/java/com/example/coderabbit_test/CoderabbitTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.coderabbit_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoderabbitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoderabbitTestApplication.class, args);
+	}
+
+}
